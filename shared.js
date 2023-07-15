7b3ee26e714a8cab88383127744cc4470d10da01
Immanual <immanualatrinity@gmail.com>
2023-07-15 19:58:24 +0530

    Assignment solution

--- shared.js
+++ shared.js
@@ -1,6 +1,7 @@
 var backdrop = document.querySelector('.backdrop');
 var modal = document.querySelector('.modal');
 var selectPlanButtons = document.querySelectorAll('.plan button');
+var modalNoButton = document.querySelector('.modal__action--negative');
 
 // console.dir(backdrop);
 
@@ -10,3 +11,12 @@ for (let i = 0; i < selectPlanButtons.length; i++) {
     modal.style.display = "block";
   });
 }
+
+function closeModal () {
+  backdrop.style.display = "none";
+  modal.style.display = "none";
+}
+
+backdrop.addEventListener('click', closeModal);
+
+modalActionNoButton.addEventListener('click', closeModal);
